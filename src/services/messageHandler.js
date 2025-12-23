713a26d25dfba956badb2be30bb6c3c1a1a6b9b7
Residente2 <residencias2@valcomti.com>
2025-12-23 10:49:08 -0600

    [UPDATE]: Se modifica el nombre para solo obtener un nombre

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -3,10 +3,14 @@ import { whatsappService } from "../services/whatsappService.js";
 class MessageHandler {
   async handleIncomingMessage(message, senderInfo) {
     if (message?.type !== "text") return;
-    
+
     const from = message.from;
     const text = message.text.body;
-    const incommingMessage = message.text.body.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
+    const incommingMessage = message.text.body
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .trim();
 
     if (this.isGreeting(incommingMessage)) {
       await this.sendWelcomeMessage(message.from, message.id, senderInfo);
@@ -14,27 +18,36 @@ class MessageHandler {
       const response = `Echo: ${text}`;
       await whatsappService.sendMessage(from, response, message.id);
     }
-    
-
     await whatsappService.markAsRead(message.id);
   }
 
   isGreeting(message) {
-    const greetings = ["hola", "hi", "buenos días", "buenos dias", "buenas tardes", "buenas noches"];
+    const greetings = [
+      "hola",
+      "hi",
+      "buenos días",
+      "buenos dias",
+      "buenas tardes",
+      "buenas noches",
+    ];
     return greetings.includes(message);
   }
 
-  getSenderName(senderInfo){
+  getSenderName(senderInfo) {
     if (!senderInfo) return "Practicante de ValcomTI";
 
-    return senderInfo.profile?.name || senderInfo.wa_id || "Practicante de ValcomTI";
+    const name = senderInfo.profile?.name || senderInfo.wa_id || "Practicante de ValcomTI";
+
+    const firstName = name.split(" ")[0];
+    return firstName;
   }
 
   async sendWelcomeMessage(to, messageId, senderInfo) {
     const name = this.getSenderName(senderInfo);
 
     const welcomeMessage =
-      `Hola ${name}, Bienvenido al servicio de PETVET online. `+ "¿En que puedo ayudarte hoy?";
+      `Hola ${name}, Bienvenido al servicio de PETVET online. ` +
+      "¿En que puedo ayudarte hoy?";
 
     await whatsappService.sendMessage(to, welcomeMessage, messageId);
   }
