4ea8e236c27015e19ae0e6d6465acf9f24865acb
Residente2 <residencias2@valcomti.com>
2025-12-23 16:29:38 -0600

    [ADD]: Se agregan características para mensajes.

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -116,7 +116,7 @@ class MessageHandler {
   async sendMedia(to, type) {
     const mediaMap = {
       audio: {
-        url: "https://bucketcharlyamazon07.s3.us-east-2.amazonaws.com/public/good_moorning.mp3",
+        url: "https://s3.amazonaws.com/gndx.dev/medpet-audio.aac",
         caption: null,
       },
       image: {
