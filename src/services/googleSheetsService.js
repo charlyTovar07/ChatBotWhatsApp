718973a48b210123cdc4ba9baefdb8b9e2e23a51
Residente2 <residencias2@valcomti.com>
2025-12-24 09:45:13 -0600

    [UPDATE]: Se modifica archivo de google sheets

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -1,49 +1,43 @@
-import fs from "fs";
-import path from "path";
-import { google } from "googleapis";
+import path from 'path';
+import { google } from 'googleapis';
 
-const sheets = google.sheets("v4");
+const sheets = google.sheets('v4');
 
 async function addRowToSheet(auth, spreadsheetId, values) {
-  try {
-    const response = await sheets.spreadsheets.values.append({
-      spreadsheetId,
-      range: "reservas",
-      valueInputOption: "RAW",
-      insertDataOption: "INSERT_ROWS",
-      resource: { values: [values] },
-      auth,
-    });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+    const request = {
+        spreadsheetId,
+        range: 'reservas',
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        resource: {
+            values: [values],
+        },
+        auth,
+    }
+
+    try {
+        const response = (await sheets.spreadsheets.values.append(request).data);
+        return response;
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 const appendToSheet = async (data) => {
-  try {
-    // --- Crear archivo temporal de credenciales ---
-    const credentialsPath = path.join(process.cwd(), "credentials.json");
-    fs.writeFileSync(credentialsPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+    try {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: path.join(process.cwd(), 'src/credentials', 'credentials.json'),
+            scopes: ['https://www.googleapis.com/auth/spreadsheets']
+        });
 
-    const auth = new google.auth.GoogleAuth({
-      keyFile: credentialsPath,
-      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-    });
+        const authClient = await auth.getClient();
+        const spreadsheetId = '1qt4Adt_muJZf1LXlXjqRUcs5hDf6Zac1wzGiwHeH_Ns'
 
-    const authClient = await auth.getClient();
-    const spreadsheetId = "1tcmkAmd6-Ah1JjDnr0EMZq0FUaUcaPbXvuKb-IGb3Z8";
-
-    await addRowToSheet(authClient, spreadsheetId, data);
-    return "Datos correctamente agregados";
-  } catch (error) {
-    console.error(error);
-  } finally {
-    // --- Limpiar archivo temporal ---
-    if (fs.existsSync(path.join(process.cwd(), "credentials.json"))) {
-      fs.unlinkSync(path.join(process.cwd(), "credentials.json"));
+        await addRowToSheet(authClient, spreadsheetId, data);
+        return 'Datos correctamente agregados'
+    } catch (error) {
+        console.error(error);
     }
-  }
-};
+}
 
 export default appendToSheet;
